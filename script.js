37415484b2e468dab7c1e2a27c62caaa2deb1c95
Adarsh Saha <162476113+Adarsh2401@users.noreply.github.com>
2024-10-22 23:40:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Use getElementById to retrieve the target div element
+var targetDiv = document.getElementById("targetDiv");
+
+// Change background color to yellow
+targetDiv.style.backgroundColor = "yellow";
+
+// Add a margin of 20 pixels
+targetDiv.style.margin = "20px";
+
+// Apply padding of 10 pixels
+targetDiv.style.padding = "10px";
+
+// Change font size to 18 pixels
+targetDiv.style.fontSize = "18px";
+
+// Set font weight to bold
+targetDiv.style.fontWeight = "bold";
+
+// Change the height to 200 pixels
+targetDiv.style.height = "200px";
+
+// Modify the width to 300 pixels
+targetDiv.style.width = "300px";
